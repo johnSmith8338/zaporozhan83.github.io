14329fbe82b5c93341b398d40c543499cd0546c0
Zaporozhan Dmitriy <zaporozhan83@gmail.com>
2018-01-27 16:38:43 +0200

    Added motion and bandi porjects

---
+++ bandi/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.slick-gallery').slick({
+		arrows: false,
+		fade: true,
+		dots: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		draggable: false
+	});
+});
