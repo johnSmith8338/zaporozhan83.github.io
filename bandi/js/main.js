cdcf178e127b3d842db0a8f816ce036154196e65
Zaporozhan Dmitriy <zaporozhan83@gmail.com>
2018-01-31 19:26:26 +0200

    Completed ver of bandi

--- bandi/js/main.js
+++ bandi/js/main.js
@@ -1,10 +1,17 @@
-$(document).ready(function(){
-	$('.slick-gallery').slick({
-		arrows: false,
-		fade: true,
-		dots: true,
-		autoplay: true,
-		autoplaySpeed: 2000,
-		draggable: false
-	});
+$(document).ready(function () {
+    $('.slick-gallery').slick({
+        arrows: false,
+        fade: true,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        draggable: false
+    });
+    mobileMenu();
 });
+
+function mobileMenu() {
+    $(".hidden_menu_holder").click(function () {
+        $(".nav").toggleClass('nav-active');
+    });
+}
